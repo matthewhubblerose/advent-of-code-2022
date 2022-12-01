cdae85c8454d4f6de291891503abf96e5a74b7ba
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-01 08:54:24 +0000

    Fix 0 padding in scripts

--- create-project.sh
+++ create-project.sh
@@ -3,7 +3,7 @@
 DAY_NUMBER=${1}
 if [ -z "${DAY_NUMBER}" ];
 then
-    DAY_NUMBER=$(date +%d)
+    DAY_NUMBER=$(date +%-d)
 fi
 printf -v PADDED_DAY_NUMBER "%02d" ${DAY_NUMBER}
 
--- get-input.sh
+++ get-input.sh
@@ -3,14 +3,14 @@
 DAY_NUMBER=${1}
 if [ -z "${DAY_NUMBER}" ];
 then
-    DAY_NUMBER=$(date +%d)
+    DAY_NUMBER=$(date +%-d)
 fi
 printf -v PADDED_DAY_NUMBER "%02d" ${DAY_NUMBER}
+YEAR=$(date +%Y)
 
 if [ -z "${SESSION_ID}" ];
 then
     echo "Enter session ID:"
     read SESSION_ID
 fi
-curl -H "cookie: session=${SESSION_ID}" "https://adventofcode.com/2022/day/${DAY_NUMBER}/input" > "./day-${PADDED_DAY_NUMBER}/input.txt"
-
+curl -H "cookie: session=${SESSION_ID}" "https://adventofcode.com/${YEAR}/day/${DAY_NUMBER}/input" > "./day-${PADDED_DAY_NUMBER}/input.txt"
