2a97b0a1f8b5d87b3ceeeaeb9d8483b12370292e
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-02 06:27:33 +0000

    Day 1 using less memory

--- day-01/main.cpp
+++ day-01/main.cpp
@@ -2,19 +2,24 @@
 #include <fstream>
 #include <string>
 #include <queue>
-#include <algorithm>
+#include <functional>
 
 int main() {
     auto ifs = std::ifstream();
     ifs.open("input.txt");
 
-    auto elf_calories = std::priority_queue<int64_t>();
+    auto calories = std::priority_queue<int64_t, std::vector<int64_t>, std::greater<>>();
     auto acc = static_cast<int64_t>(0);
 
     auto line = std::string();
     while (std::getline(ifs, line)) {
         if (line.empty()) {
-            elf_calories.push(acc);
+            if (calories.size() < 3) {
+                calories.push(acc);
+            } else if (acc > calories.top()) {
+                calories.pop();
+                calories.push(acc);
+            }
             acc = 0;
         } else {
             acc += std::stol(line);
@@ -22,11 +27,9 @@ int main() {
     }
 
     auto total = static_cast<int64_t>(0);
-    auto count = 0;
-    while (!elf_calories.empty() && count < 3) {
-        total += elf_calories.top();
-        elf_calories.pop();
-        count++;
+    while (!calories.empty()) {
+        total += calories.top();
+        calories.pop();
     }
 
     std::cout << total << '\n';
