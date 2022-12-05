bd3d11dc8068ef3c0c42aff94996afb45a0b200b
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-05 10:39:44 +0000

    Day 5 part 2

--- day-05/main.cpp
+++ day-05/main.cpp
@@ -4,35 +4,35 @@
 #include <sstream>
 #include <deque>
 
-bool get_crate(std::string_view sv, size_t &index, char &crate) {
-    if (index >= sv.length()) { return false; }
-    crate = sv[index + 1];
-    index += 4;
+bool get_crate(std::string_view sv, size_t &out_index, char &out_crate) {
+    if (out_index >= sv.length()) return false;
+    const char crate = sv[out_index + 1];
+    out_crate = crate;
+    out_index += 4;
     return true;
 }
 
-void part_1() {
+std::vector<std::deque<char>> parse_stacks() {
     auto stacks = std::vector<std::deque<char>>();
-
     std::string line;
-    // Read stacks
+
     while (std::getline(std::cin, line) && line[1] != '1') {
         auto ss = std::istringstream(line);
         char crate;
-        size_t stack = 0;
-        size_t index = 0;
-        while (get_crate(line, index, crate)) {
-            if (stacks.size() < stack + 1) {
-                stacks.emplace_back();
-            }
-            if (crate != ' ') {
-                stacks[stack].push_front(crate);
-            }
-            ++stack;
+        size_t stack_index = 0, line_index = 0;
+        while (get_crate(line, line_index, crate)) {
+            if (stacks.size() < stack_index + 1) stacks.emplace_back();
+            if (crate != ' ') stacks[stack_index].push_front(crate);
+            ++stack_index;
         }
     }
+    std::getline(std::cin, line); // skip stack numbers line
+    return stacks;
+}
 
-    // Read instructions and process
+void part_1() {
+    auto stacks = parse_stacks();
+
     std::string s;
     size_t count, src, dst;
     while (std::cin >> s >> count >> s >> src >> s >> dst) {
@@ -50,11 +50,26 @@ void part_1() {
 }
 
 void part_2() {
+    auto stacks = parse_stacks();
 
+    std::string s;
+    size_t count, src_num, dst_num;
+    while (std::cin >> s >> count >> s >> src_num >> s >> dst_num) {
+        auto &src = stacks[src_num - 1];
+        auto &dst = stacks[dst_num - 1];
+        const auto index = static_cast<std::ptrdiff_t>(src.size() - count);
+        std::copy(src.begin() + index, src.end(), std::back_inserter(dst));
+        src.erase(src.begin() + index, src.end());
+
+    }
+    for (const auto &stack: stacks) {
+        std::cout << stack.back();
+    }
+    std::cout << '\n';
 }
 
 int main() {
-    part_1();
-    part_2();
+//    part_1(); // QGTHFZBHV
+    part_2(); // MGDMPSZTM
     return 0;
 }
