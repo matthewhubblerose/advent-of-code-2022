1ad6cdd73b28d4d343d1f4e702d884aea4ac3371
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-05 23:19:21 +0000

    Day 5 cleanup.

--- day-05/main.cpp
+++ day-05/main.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <string>
-#include <sstream>
 #include <deque>
-
-bool get_crate(std::string_view sv, size_t &out_index, char &out_crate) {
-    if (out_index >= sv.length()) return false;
-    const char crate = sv[out_index + 1];
-    out_crate = crate;
-    out_index += 4;
-    return true;
-}
 
 std::vector<std::vector<char>> parse_stacks() {
     auto stacks = std::vector<std::vector<char>>();
     std::string line;
 
     while (std::getline(std::cin, line) && line[1] != '1') {
-        auto ss = std::istringstream(line);
-        char crate;
-        size_t stack_index = 0, line_index = 0;
-        while (get_crate(line, line_index, crate)) {
+        for (size_t i = 0; i < line.size(); i += 4) {
+            const auto crate = line[i + 1];
+            const auto stack_index = i / 4;
             if (stacks.size() < stack_index + 1) stacks.emplace_back();
             if (crate != ' ') stacks[stack_index].insert(stacks[stack_index].cbegin(), crate);
-            ++stack_index;
         }
     }
     std::getline(std::cin, line); // skip stack numbers line
@@ -64,7 +52,7 @@ void part_2() {
 }
 
 int main() {
-    part_1(); // QGTHFZBHV
-//    part_2(); // MGDMPSZTM
+//    part_1(); // QGTHFZBHV
+    part_2(); // MGDMPSZTM
     return 0;
 }
