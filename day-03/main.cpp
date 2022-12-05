1633ffe725eac935b066f750f5eaaeddb218401f
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-05 22:43:38 +0000

    Day 3 cleanup.

--- day-03/main.cpp
+++ day-03/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-#include <fstream>
 #include <vector>
 #include <string>
 #include <set>
 
 std::set<char> to_char_set(std::string_view sv) {
-    auto set = std::set<char>();
+    std::set<char> set;
     for (const auto c: sv) {
         set.insert(c);
     }
@@ -20,34 +19,50 @@ int score(char c) {
     }
 }
 
-int main() {
-    auto ifs = std::ifstream();
-    ifs.open("input.txt");
+template<typename T, typename C1, typename C2>
+std::vector<T> set_intersection(const C1 &a, const C2 &b) {
+    std::vector<T> i;
+    std::set_intersection(a.begin(), a.end(), b.begin(), b.end(), std::back_inserter(i));
+    return i;
+}
 
-    auto acc = static_cast<uint64_t>(0);
+void part_1() {
+    int acc = 0;
+    std::string line;
 
-    auto line = std::string();
-    auto line_2 = std::string();
-    auto line_3 = std::string();
-    while (std::getline(ifs, line)) {
-        std::getline(ifs, line_2);
-        std::getline(ifs, line_3);
-
-        const auto chars_1 = to_char_set(line);
-        const auto chars_2 = to_char_set(line_2);
-        const auto chars_3 = to_char_set(line_3);
-
-        auto intersection_1 = std::vector<char>();
-        std::set_intersection(chars_1.begin(), chars_1.end(), chars_2.begin(), chars_2.end(),
-                              std::back_inserter(intersection_1));
-        auto intersection_2 = std::vector<char>();
-        std::set_intersection(intersection_1.begin(), intersection_1.end(), chars_3.begin(), chars_3.end(),
-                              std::back_inserter(intersection_2));
-
-        assert(intersection_2.size() == 1);
-        acc += static_cast<uint64_t>(score(intersection_2[0]));
+    while (std::cin >> line) {
+        const auto s = std::string_view(line);
+        const auto pack_1 = to_char_set(s.substr(0, s.size() / 2));
+        const auto pack_2 = to_char_set(s.substr(s.size() / 2, s.size()));
+        const auto intersection = set_intersection<char>(pack_1, pack_2);
+        assert(intersection.size() == 1);
+        acc += score(intersection[0]);
     }
 
     std::cout << acc << '\n';
+}
+
+void part_2() {
+    int acc = 0;
+    std::string line_1, line_2, line_3;
+
+    while (std::cin >> line_1 >> line_2 >> line_3) {
+        const auto chars_1 = to_char_set(line_1);
+        const auto chars_2 = to_char_set(line_2);
+        const auto chars_3 = to_char_set(line_3);
+
+        auto intersection_1 = set_intersection<char>(chars_1, chars_2);
+        auto intersection_2 = set_intersection<char>(intersection_1, chars_3);
+
+        assert(intersection_2.size() == 1);
+        acc += score(intersection_2[0]);
+    }
+
+    std::cout << acc << '\n';
+}
+
+int main() {
+//    part_1(); // 7674
+    part_2(); // 2805
     return 0;
 }
