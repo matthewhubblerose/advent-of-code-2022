c1e378b01069ff90bcc05922e52d517d343c6afe
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-07 22:57:15 +0000

    Dat 7 part 2 and some cleanup

--- day-07/main.cpp
+++ day-07/main.cpp
@@ -1,110 +1,75 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
-#include <string>
 #include <numeric>
-
-struct File {
-    int size;
-    std::string name;
-
-    File(int size, std::string name) : size(size), name(std::move(name)) {}
-};
 
 struct Dir {
     Dir *parent;
-    std::string name;
-    std::vector<std::unique_ptr<Dir>> dirs;
-    std::vector<std::unique_ptr<File>> files;
+    std::vector<Dir> dirs;
+    int files_size = 0;
 
-    explicit Dir(Dir *parent, std::string name) : parent(parent), name(std::move(name)) {}
+    explicit Dir(Dir *parent) : parent(parent) {}
 
     [[nodiscard]] int size() const {
-        int acc = 0;
-        for (const auto &file: files) {
-            acc += file->size;
-        }
-        for (const auto &dir: dirs) {
-            acc += dir->size();
-        }
-        return acc;
+        const auto dirs_size = std::accumulate(dirs.begin(), dirs.end(), 0, [](const int acc, const Dir &dir) {
+            return acc + dir.size();
+        });
+        return files_size + dirs_size;
     }
 };
 
-int filtered_size(const Dir &dir) {
+int summed_small_dir_sizes(const Dir &dir) {
     int acc = 0;
     const auto size = dir.size();
-    if (size <= 100000) {
-        acc += size;
-    }
-    for (const auto &item: dir.dirs) {
-        acc += filtered_size(*item);
+    if (size <= 100000) acc += size;
+    for (const auto &child_dir: dir.dirs) {
+        acc += summed_small_dir_sizes(child_dir);
     }
     return acc;
 }
 
-void deletable_dirs(const Dir &dir, int min_size, std::vector<int> &sizes) {
+void smallest_deletable_dir_size(const Dir &dir, int needed_space, int &out_size) {
     const auto size = dir.size();
-    if (size >= min_size) {
-        sizes.push_back(size);
-        for (const auto &item: dir.dirs) {
-            deletable_dirs(*item, min_size, sizes);
-        }
+    if (size < needed_space) return;
+    if (size < out_size) out_size = size;
+    for (const auto &child_dir: dir.dirs) {
+        smallest_deletable_dir_size(child_dir, needed_space, out_size);
     }
 }
 
 std::unique_ptr<Dir> parse_dirs() {
-    std::string line;
-
-    auto root_dir = std::make_unique<Dir>(nullptr, "");
+    auto root_dir = std::make_unique<Dir>(nullptr);
     auto *current_dir = root_dir.get();
 
+    std::string line;
     while (std::getline(std::cin, line)) {
-        auto ss = std::istringstream(line);
-        if (line.starts_with("$ cd")) {
-            std::string dir_name;
-            ss >> dir_name >> dir_name >> dir_name;
-            if (dir_name == "/") {
-                current_dir = root_dir.get();
-            } else if (dir_name == "..") {
-                current_dir = current_dir->parent;
-            } else {
-                // check if dir already exists?
-                auto dir = std::make_unique<Dir>(current_dir, dir_name);
-                auto *p = dir.get();
-                current_dir->dirs.push_back(std::move(dir));
-                current_dir = p;
-            }
-        } else if (line.starts_with("$ ls")) {
-            // do nothing
-        } else if (line.starts_with("dir")) {
-            // do nothing
-        } else {
+        if (line == "$ cd ..") {
+            current_dir = current_dir->parent;
+        } else if (line.starts_with("$ cd")) {
+            current_dir->dirs.emplace_back(current_dir);
+            current_dir = &current_dir->dirs.back();
+        } else if (std::isdigit(line[0])) {
             int size;
-            std::string file_name;
-            ss >> size >> file_name;
-            current_dir->files.push_back(std::make_unique<File>(size, file_name));
+            std::istringstream(line) >> size;
+            current_dir->files_size += size;
         }
     }
     return root_dir;
 }
 
 void part_1() {
-    const auto root_dir = parse_dirs();
-    const auto foo = filtered_size(*root_dir);
-    std::cout << foo << '\n';
+    std::cout << summed_small_dir_sizes(*parse_dirs()) << '\n';
 }
 
 void part_2() {
     const auto root_dir = parse_dirs();
-    const auto free_space = 70000000 - root_dir->size();
-    std::vector<int> sizes;
-    deletable_dirs(*root_dir, free_space, sizes);
-    std::cout << *std::min_element(sizes.cbegin(), sizes.cend()) << '\n';
+    int size = std::numeric_limits<int>::max();
+    smallest_deletable_dir_size(*root_dir, root_dir->size() - 40000000, size);
+    std::cout << size << '\n';
 }
 
 int main() {
 //    part_1(); // 1644735
-    part_2();
+    part_2(); // 1300850
     return 0;
 }
