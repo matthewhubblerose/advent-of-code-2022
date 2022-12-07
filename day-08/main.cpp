a7d36ab9dbfa918a356240c7ff6c60596b4a6285
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-07 23:00:11 +0000

    Added day 8

---
+++ day-08/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <algorithm>
+
+void part_1() {
+    std::string line;
+    while (std::cin >> line) {
+//        auto ss = std::istringstream(line);
+//        auto token = std::string();
+//        std::getline(ss, token, ' ');
+
+//        auto token = std::string();
+//        while (std::getline(ss, token, ' ')) {
+//        }
+    }
+
+    std::cout << "" << '\n';
+}
+
+void part_2() {
+
+}
+
+int main() {
+    part_1();
+    part_2();
+    return 0;
+}
