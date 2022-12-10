f03582f7e3248081a655e0c699bf3853072e792e
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-10 07:31:41 +0000

    Day 10

--- day-10/main.cpp
+++ day-10/main.cpp
@@ -1,36 +1,59 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <map>
-#include <set>
-#include <unordered_map>
-#include <unordered_set>
-#include <queue>
-#include <deque>
-#include <stack>
-#include <algorithm>
+
+int add_value(int cycle, int x) {
+    return (cycle - 20) % 40 == 0 ? cycle * x : 0;
+}
 
 void part_1() {
-    std::string line;
-    while (std::cin >> line) {
-//        auto ss = std::istringstream(line);
-//        auto token = std::string();
-//        std::getline(ss, token, ' ');
+    std::string instruction;
+    int value, x = 1, cycle = 0, total = 0;
 
-//        auto token = std::string();
-//        while (std::getline(ss, token, ' ')) {
-//        }
+    while (std::cin >> instruction) {
+        if (instruction == "noop") {
+            ++cycle;
+           total += add_value(cycle, x);
+        } else {
+            std::cin >> value;
+            ++cycle;
+            total += add_value(cycle, x);
+            ++cycle;
+            total += add_value(cycle, x);
+            x += value;
+        }
     }
 
-    std::cout << "" << '\n';
+    std::cout << total << '\n';
+}
+
+void print_pixel(int cycle, int x) {
+    const int pos = cycle % 40;
+    if (pos == 0) std::cout << '\n';
+    const char c = (x >= pos - 1 && x <= pos + 1) ? '#' : '.';
+    std::cout << c;
 }
 
 void part_2() {
+    std::string instruction;
+    int value, x = 1, cycle = 0;
 
+    while (std::cin >> instruction) {
+        if (instruction == "noop") {
+            print_pixel(cycle, x);
+            ++cycle;
+        } else {
+            std::cin >> value;
+            print_pixel(cycle, x);
+            ++cycle;
+            print_pixel(cycle, x);
+            ++cycle;
+            x += value;
+        }
+    }
 }
 
 int main() {
-    part_1();
-    part_2();
+//    part_1(); // 14240
+    part_2(); // PLULKBZH
     return 0;
 }
