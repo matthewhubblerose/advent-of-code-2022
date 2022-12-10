fb0d254a84183911aa3b173bfc3302d4081d244c
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-10 13:38:53 +0000

    Day 10 part 1 & 2 in 198 chars ⛳️

--- day-10/main_golf.cpp
+++ day-10/main_golf.cpp
@@ -1,2 +1,2 @@
 #include<iostream>
-using namespace std;char i[5];int v,x=1,c,t,p;void a(){if(!p)cout<<'\n';cout<<".#"[x>p-2&x<p+2];p=++c%40;t+=p==20?c*x:0;}int main(){while(cin>>i){a();if(*i&1)cin>>v,a(),x+=v;}cout<<t;}
+char i[5];int v,x=1,c,t,p;void a(){if(!p)puts("");putchar(".#"[x>p-2&x<p+2]);p=++c%40;t+=p==20?c*x:0;}int main(){while(std::cin>>i){a();if(*i&1)std::cin>>v,a(),x+=v;}std::cout<<t;}
