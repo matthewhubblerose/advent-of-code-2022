d812560c5a42b28ddb37422e4c89e7d125f999e7
Matthew Hubble-Rose <mhubblerose@spotify.com>
2022-12-13 21:35:12 +0000

    Day 13 cleanup

--- day-13/main.cpp
+++ day-13/main.cpp
@@ -2,108 +2,97 @@
 #include <vector>
 #include <string>
 #include <stack>
+#include <charconv>
+
+template<typename C, typename T>
+typename C::difference_type index_of(const C &v, const T &f) {
+    const auto it = std::find(v.cbegin(), v.cend(), f);
+    if (it == v.cend()) return -1;
+    return std::distance(v.begin(), it);
+}
+
+template<typename T>
+int compare(const T &l, const T &r) { return (l < r) ? -1 : (l > r) ? 1 : 0; }
+
+int svtoi(std::string_view sv) {
+    int i;
+    const auto res = std::from_chars(sv.begin(), sv.end(), i);
+    assert(static_cast<int>(res.ec) == 0);
+    return i;
+}
 
 struct Packet {
-    std::vector<std::unique_ptr<Packet>> children;
+    std::vector<Packet> children;
     int val = -1;
 };
 
-std::unique_ptr<Packet> parse_packet(std::string_view s) {
+Packet packet_with_child(int val) {
+    auto p = Packet();
+    p.children.emplace_back().val = val;
+    return p;
+}
+
+Packet parse(std::string_view s) {
+    s = s.substr(1, s.size() - 2); // optional - ignore opening and closing []
     auto stack = std::stack<Packet*>();
-    auto packet = std::make_unique<Packet>();
-    auto current = packet.get();
+    auto packet = Packet();
+    auto current = &packet;
     for (size_t i = 0; i < s.size(); ++i) {
         const char c = s[i];
         if (c == '[') {
-            auto child = current->children.emplace_back(std::make_unique<Packet>()).get();
+            auto &child = current->children.emplace_back();
             stack.push(current);
-            current = child;
+            current = &child;
         } else if (c == ']') {
             current = stack.top();
             stack.pop();
         } else if (c != ',') {
             const auto start = i;
-            while (i < s.size() - 1 && std::isdigit(s[i + 1])) ++i;
-            const int val = std::stoi(std::string(s.substr(start, (i + 1) - start)));
-            auto child = current->children.emplace_back(std::make_unique<Packet>()).get();
-            child->val = val;
+            size_t len = 1;
+            while (i + len < s.size() && std::isdigit(s[i + len])) ++len;
+            i += len;
+            current->children.emplace_back().val = svtoi(s.substr(start, len));
         }
     }
     return packet;
 }
 
 int order(const Packet &p1, const Packet &p2) {
-    if (p1.val >= 0 && p2.val >= 0) {
-        // both values are integers
-        if (p1.val < p2.val) return -1;
-        else if (p1.val > p2.val) return 1;
-        else return 0;
+    if (p1.val != -1 && p2.val != -1) {
+        return compare(p1.val, p2.val);
     } else if (p1.val == -1 && p2.val == -1) {
-        // both values are lists
         const auto size = std::min(p1.children.size(), p2.children.size());
         for (size_t i = 0; i < size; ++i) {
-            const auto o = order(*p1.children[i], *p2.children[i]);
-            if (o != 0) return o;
+            const auto cmp = order(p1.children[i], p2.children[i]);
+            if (cmp != 0) return cmp;
         }
-        if (p1.children.size() < p2.children.size()) return -1;
-        else if (p1.children.size() > p2.children.size()) return 1;
-        else return 0;
+        return compare(p1.children.size(), p2.children.size());
     } else {
-        // one is an integer, one is a list
-        if (p1.val == -1) {
-            auto l2 = std::make_unique<Packet>();
-            auto *child = l2->children.emplace_back(std::make_unique<Packet>()).get();
-            child->val = p2.val;
-            return order(p1, *l2);
-        } else {
-            auto l1 = std::make_unique<Packet>();
-            auto *child = l1->children.emplace_back(std::make_unique<Packet>()).get();
-            child->val = p1.val;
-            return order(*l1, p2);
-        }
+        if (p1.val != -1) return order(packet_with_child(p1.val), p2);
+        else              return order(p1, packet_with_child(p2.val));
     }
-}
-
-bool in_order(const Packet &p1, const Packet &p2) {
-    return order(p1, p2) == -1;
 }
 
 void part_1() {
-    std::string line_1, line_2;
     int index = 1;
     int total = 0;
+    std::string line_1, line_2;
     while (std::cin >> line_1 >> line_2) {
-        auto packet_1 = parse_packet(line_1);
-        auto packet_2 = parse_packet(line_2);
-        if (in_order(*packet_1, *packet_2)) {
-            total += index;
-        }
+        if (order(parse(line_1), parse(line_2)) == -1) total += index;
         ++index;
     }
-
     std::cout << total << '\n';
 }
 
 void part_2() {
-    std::vector<std::string> packets;
-    packets.emplace_back("[[2]]");
-    packets.emplace_back("[[6]]");
-
+    using namespace std::string_literals;
+    std::vector<std::string> p = {"[[2]]", "[[6]]"};
     std::string line_1, line_2;
-    while (std::cin >> line_1 >> line_2) {
-        packets.push_back(line_1);
-        packets.push_back(line_2);
-    };
-
-    std::sort(packets.begin(), packets.end(), [](const std::string &l, const std::string &r) {
-        const auto p1 = parse_packet(l);
-        const auto p2 = parse_packet(r);
-        return in_order(*p1, *p2);
+    while (std::cin >> line_1 >> line_2) { p.push_back(line_1); p.push_back(line_2); }
+    std::sort(p.begin(), p.end(), [](const std::string &l, const std::string &r) {
+        return order(parse(l), parse(r)) == -1;
     });
-
-    const auto start = std::distance(packets.begin(), std::find(packets.begin(), packets.end(), "[[2]]")) + 1;
-    const auto end = std::distance(packets.begin(), std::find(packets.begin(), packets.end(), "[[6]]")) + 1;
-    std::cout << (start * end) << '\n';
+    std::cout << ((index_of(p, "[[2]]"s) + 1) * (index_of(p, "[[6]]"s) + 1)) << '\n';
 }
 
 int main() {
